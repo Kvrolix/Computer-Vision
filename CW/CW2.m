07d0c89a9c4dc87cf04c055f634c1f2ec332c745
Kvrolix <filipfilip306@gmail.com>
2023-11-20 13:59:18 +0000

    1

---
+++ CW/CW2.m
@@ -0,0 +1,33 @@
+
+clear;
+% Load the first image and the second image
+img_start =imread("002.jpg");
+img_end = imread("011.jpg");
+
+
+%There should be a warning from the system if 
+
+% 1) Car > 2.5m width
+% 2) Speed > 30 mph
+% 3) Therefore: Car > 2.5m width || Speed > 30 mph
+
+% the only exempt are the fire trucks of wide length radio 1:3
+% are mainly red colour AND have a width length ratio to 1:3, 1 width is ==
+% 3 lenghts
+
+
+
+
+
+
+
+
+%Basic functionality 
+% 1) Detect car size 
+% 2) Detect speed 
+% 3) Detect type of the car
+
+
+% if the image is a fire truck
+
+% if the car is oversized 
